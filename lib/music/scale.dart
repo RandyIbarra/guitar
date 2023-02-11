feb6cbb6ce01cacd36130537e4971e4c71ee816b
Randy Ibarra <randyosbaldo@gmail.com>
2023-02-11 01:38:03 -0600

    feat: functions to Scale class

--- lib/music/scale.dart
+++ lib/music/scale.dart
@@ -1,21 +1,22 @@
 import 'package:guitar/music/note.dart';
 
 class Scale {
-  List<String> notes;
+  Note key;
+  List<Note> notes;
 
-  Scale.internal(this.notes);
+  Scale.internal(this.key, this.notes);
 
   factory Scale(Note key, List<int> code) {
-    List<String> notes = [key.name];
+    List<Note> notes = [key];
     Note note = key;
     for (int semitones in code) {
       note = note + semitones;
-      notes.add(note.name);
+      notes.add(note);
     }
     if (notes.first == notes.last) {
       notes.removeLast();
     }
-    return Scale.internal(notes);
+    return Scale.internal(key, notes);
   }
 
   factory Scale.getMajor(Note key) {
@@ -25,4 +26,26 @@ class Scale {
   factory Scale.getMinor(Note key) {
     return Scale(key, <int>[2, 1, 2, 2, 1, 2, 2]);
   }
+
+  factory Scale.getChromatic(Note key) {
+    return Scale(key, <int>[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  }
+
+  bool hasNote(Note queryNote) {
+    for (Note note in notes) {
+      if (note.isEqualTo(queryNote)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool isKey(Note note) {
+    return notes[0].isEqualTo(note);
+  }
 }
+
+List<String> modes = [
+  'major',
+  'minor',
+];
