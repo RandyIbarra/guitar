c02f901572b9d3400b6dae714d1d4082ca47e110
Randy Ibarra <randyosbaldo@gmail.com>
2023-02-11 01:38:27 -0600

    feat: functions to Chord class

--- lib/music/chord.dart
+++ lib/music/chord.dart
@@ -1,22 +1,33 @@
 import 'package:guitar/music/note.dart';
 
 class Chord {
-  List<String> notes;
+  Note key;
+  List<Note> notes;
 
-  Chord.internal(this.notes);
+  Chord.internal(this.key, this.notes);
 
   factory Chord(Note key, String mode) {
-    List<String> notes = [key.name];
+    List<Note> notes = [key];
     switch (mode) {
       case 'major':
-        notes.add((key + 4).name);
-        notes.add((key + 7).name);
+        notes.add(key + 4);
+        notes.add(key + 7);
         break;
       case 'minor':
-        notes.add((key + 3).name);
-        notes.add((key + 7).name);
+        notes.add(key + 3);
+        notes.add(key + 7);
         break;
     }
-    return Chord.internal(notes);
+    return Chord.internal(key, notes);
+  }
+
+  int hasNote(Note queryNote) {
+    for (int index = 0; index < 3; index++) {
+      Note note = notes[index];
+      if (note.isEqualTo(queryNote)) {
+        return index * 2 + 1;
+      }
+    }
+    return 0;
   }
 }
