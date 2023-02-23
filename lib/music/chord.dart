da58a522c4fdfb44fb16406943eede06f4285ea2
Randy Ibarra <randyosbaldo@gmail.com>
2023-02-22 22:31:14 -0600

    feat: chordTypeRegistry and comments added

--- lib/music/chord.dart
+++ lib/music/chord.dart
@@ -1,24 +1,43 @@
 import 'package:guitar/music/note.dart';
 
+/// Modes of a chord.
+List<String> chordModes = [
+  'major',
+  'minor',
+];
+
+/// Dart implementation of a chord.
+/// A [Chord] consists of 3 [Note]'s (1st, 3rd, 5th) or 4 [Note]'s
+/// (1st, 3rd, 5th, 7th) with key [Note] as first.
 class Chord {
+  /// Key note of chord.
   Note key;
+
+  /// Notes of chord.
   List<Note> notes;
 
-  Chord.internal(this.key, this.notes);
+  Chord(this.key, this.notes);
 
-  factory Chord(Note key, String mode) {
+  /// Major chords constructor from key note.
+  factory Chord.getMajor(Note key) {
     List<Note> notes = [key];
-    switch (mode) {
-      case 'major':
-        notes.add(key + 4);
-        notes.add(key + 7);
-        break;
-      case 'minor':
-        notes.add(key + 3);
-        notes.add(key + 7);
-        break;
-    }
-    return Chord.internal(key, notes);
+    notes.add(key + 4);
+    notes.add(key + 7);
+    return Chord(key, notes);
+  }
+
+  /// Minor chords constructor from key note.
+  factory Chord.getMinor(Note key) {
+    List<Note> notes = [key];
+    notes.add(key + 3);
+    notes.add(key + 7);
+    return Chord(key, notes);
+  }
+
+  factory Chord.getChordFromMode(Note key, String mode, {int nNotes = 3}) {
+    final constructor = chordRegistry[mode];
+    final chord = constructor!(key);
+    return chord;
   }
 
   int hasNote(Note queryNote) {
@@ -31,3 +50,10 @@ class Chord {
     return 0;
   }
 }
+
+/// You can get the constructor by var _constructor = chordRegistry['major'].
+/// And then, make your major chord by var _chord = _constructor(Note('C')).
+Map<String, Chord Function(Note)> chordRegistry = {
+  "major": (note) => Chord.getMajor(note),
+  "minor": (note) => Chord.getMinor(note),
+};
